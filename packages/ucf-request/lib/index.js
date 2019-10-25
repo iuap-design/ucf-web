5aab23e4ffd22f1e71debb31e6c31ce676a67657
liushaozhen <804199994@qq.com>
2019-10-25 17:19:55 +0800

    错误提示

--- packages/ucf-request/lib/index.js
+++ packages/ucf-request/lib/index.js
@@ -3,21 +3,25 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
+exports["default"] = void 0;
 
 var _axios = _interopRequireDefault(require("axios"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var x_xsrf_token = '',
     random_num = Math.random();
 
+var noop = function noop() {};
+
 var _default = function _default(url, options) {
-  options['start'] && options['start']();
+  var startFunc = options['start'] || noop;
+  var endFunc = options['end'] || noop;
+  startFunc();
   var params = Object.assign({}, options.params, options.method.toLowerCase() == 'get' ? {
     r: Math.random()
   } : {});
-  return (0, _axios.default)({
+  var defaultOptions = {
     timeout: 8000,
     method: options.method,
     url: url,
@@ -28,8 +32,9 @@ var _default = function _default(url, options) {
       'x-xsrf-token': x_xsrf_token
     },
     params: params
-  }).then(function (res) {
-    options['end'] && options['end']();
+  };
+  return (0, _axios["default"])(Object.assign(defaultOptions, options)).then(function (res) {
+    endFunc();
     var inner_x_xsrf_token = res.headers['x-xsrf-token'];
 
     if (inner_x_xsrf_token) {
@@ -39,37 +44,14 @@ var _default = function _default(url, options) {
     return new Promise(function (resolve, reject) {
       resolve(res.data);
     });
-  }).catch(function (err) {
-    options['end'] && options['end']();
+  })["catch"](function (err) {
+    endFunc();
     console.log(err);
-    var res = err.response;
-
-    if (res) {
-      var _status = res.status,
-          msg = res.data.msg;
-
-      switch (_status) {
-        case 401:
-          console.log("RBAC鉴权失败!" + msg);
-          return Promise.resolve(res);
-
-        case 306:
-          window.top.location.href = '/wbalone/pages/login/login.html';
-          break;
-
-        default:
-      }
-    }
-
     return new Promise(function (resolve, reject) {
-      var errMsg = "request\u53D1\u751F\u670D\u52A1\u5668 http ".concat(status, " \u8BF7\u6C42\u9519\u8BEF!");
-      reject({
-        code: -1,
-        data: [],
-        message: errMsg
-      });
+      var errMsg = "request Error! http ".concat(status);
+      reject(err);
     });
   });
 };
 
-exports.default = _default;
+exports["default"] = _default;
--- packages/ucf-request/src/index.js
+++ packages/ucf-request/src/index.js
@@ -8,16 +8,19 @@ import axios from "axios";
 let x_xsrf_token = '',
     random_num = Math.random();
 
+const noop = ()=>{}
 /**
  * 参数：url 请求的服务地址
  * 参数：options 请求参数选项，包括 method 方法类型、params GET参数、data POST参数
  */
 export default (url, options) => {
-    options['start'] && options['start']();
+    let startFunc = options['start']||noop;
+    let endFunc = options['end']||noop;
+    startFunc()
     let params = Object.assign({}, options.params, options.method.toLowerCase() == 'get' ? {
         r: Math.random()
     } : {});
-    return axios({
+    let defaultOptions = {
         timeout: 8000,
         method: options.method,
         url: url,
@@ -25,13 +28,13 @@ export default (url, options) => {
         headers: {
             'X-Requested-With': 'XMLHttpRequest',
             'random-num': random_num,
-            'x-xsrf-token': x_xsrf_token
+            'x-xsrf-token': x_xsrf_token,
         },
         params,
-
-    }).then(function (res) {
-        // console.log('axios res', res);
-        options['end'] && options['end']();
+    }
+    
+    return axios(Object.assign(defaultOptions,options)).then(function (res) {
+        endFunc()
         let inner_x_xsrf_token = res.headers['x-xsrf-token'];//added by yany
         if (inner_x_xsrf_token) {
             x_xsrf_token = inner_x_xsrf_token;
@@ -40,24 +43,24 @@ export default (url, options) => {
             resolve(res.data);
         });
     }).catch(function (err) {
-        options['end'] && options['end']();
+        endFunc()
         console.log(err);
-        let res = err.response;
-        if (res) {
-            let { status, data: { msg } } = res;
-            switch (status) {
-                case 401:
-                    console.log("RBAC鉴权失败!" + msg);
-                    return Promise.resolve(res);
-                case 306:
-                    window.top.location.href = '/wbalone/pages/login/login.html';
-                    break;
-                default:
-            }
-        }
+        // let res = err.response;
+        // if (res) {
+        //     let { status, data: { msg } } = res;
+        //     switch (status) {
+        //         case 401:
+        //             console.log("RBAC鉴权失败!" + msg);
+        //             return Promise.resolve(res);
+        //         case 306:
+        //             window.top.location.href = '/wbalone/pages/login/login.html';
+        //             break;
+        //         default:
+        //     }
+        // }
         return new Promise((resolve, reject) => {
-            let errMsg = `request发生服务器 http ${status} 请求错误!`;
-            reject({ code: -1, data: [], message: errMsg });
+            let errMsg = `request Error! http ${status}`;
+            reject(err);
         });
     });
 }
