ef5eba7575d2222b85f582df9da36de9ce1365c2
Yogi,Wang <wang1084@126.com>
2021-09-22 16:34:36 +0800

    [fixed] (babel-config) fix compatibility
    Signed-off-by: Yogi,Wang <wang1084@126.com>

--- packages/ucf-scripts/src/base.config.js
+++ packages/ucf-scripts/src/base.config.js
@@ -57,7 +57,15 @@ const config = {
                 options: {
                     babelrc: false,
                     presets: [
-                        require.resolve('@babel/preset-env'),
+                        [require.resolve('@babel/preset-env'), {
+                          targets: {
+                            "edge": "17",
+                            "firefox": "60",
+                            "chrome": "67",
+                            "safari": "11.1",
+                            "ie": "11"
+                          }
+                        }],
                         require.resolve('@babel/preset-react'),
                         ...cfg.babel_presets
                     ],
